4b72ff1e885ec5034203251652897e38fadf18e1
Avigithub8 <sharma.avi844@gmail.com>
2023-12-13 01:45:56 +0530

    encrypted password

--- expensetracker-project/routes/userRoutes.js
+++ expensetracker-project/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Op } = require("sequelize");
 const expenseModel = require("../models/expenseModel");
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
   res.render("homeScreen");
@@ -20,13 +21,16 @@ router.post("/signup", async (req, res) => {
     if (existingUser) {
       return res.redirect("/signin");
     }
+    bcrypt.hash(password,8,async(err,password)=>{
+      console.log(err);
+      const newUser = await expenseModel.create({ username, emailid, password:password });
+      return res.render("signupScreen", {
+        message: "Signup successful",
+        user: newUser,
+      });
+    })
 
-    const newUser = await expenseModel.create({ username, emailid, password });
-
-    return res.render("signupScreen", {
-      message: "Signup successful",
-      user: newUser,
-    });
+    
   } catch (error) {
     console.error("Error during signup:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -47,11 +51,19 @@ router.post("/signin", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    if (user && user.password === password) {
-      return res.status(200).json({ message: "User login successful" });
-    } else {
-      return res.status(401).json({ message: "User not authorized" });
-    }
+    // if (user && user.password === password) {
+    bcrypt.compare(password, user.password, (err, result) => {
+      if(err){
+        throw new Error('Something went wrong!')
+      }
+      if (result==true) {
+        return res.status(200).json({ message: "User login successful" });
+      } else {
+        return res.status(400).json({ message: "Password is incorrect" });
+      }
+     
+    })
+   
   } catch (error) {
     console.error("Error during login:", error);
     res.status(500).json({ error: "Internal Server Error" });
